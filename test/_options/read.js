3d1fb7c2d938b39314005bfbe2ceae31fa967265
Mariusz Nowak <mariusz@medikoo.com>
2012-08-04 18:16:18 +0200

    _options/read - reads and calculates options
    Reads and calculates options saved in '.lint' config files

---
+++ test/_options/read.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var isCopy  = require('es5-ext/lib/Object/is-copy')
+  , resolve = require('path').resolve
+  , inspect = require('util').inspect
+
+  , path = resolve(__dirname, '../__playground/read/raz/dwa/foo.js');
+
+module.exports = function (t, a, d) {
+	t(path, true)(function (opts) {
+		var copy = {
+			adsafe: true,
+			predef: ['foo', 'last', 'marko', 'prelast', 'raz'],
+			es5: true,
+			forin: true,
+			fragment: true,
+			nomen: true
+		};
+
+		// console.log("OPTS", inspect(opts, false, Infinity));
+		// console.log("COPY", inspect(copy, false, Infinity));
+		a(isCopy(opts, copy, Infinity), true);
+	}).end(d, d);
+};
