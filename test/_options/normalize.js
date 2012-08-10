c7a7e76e7695cd1e6ad5edd4aa16aed91599f816
Mariusz Nowak <mariusz@medikoo.com>
2012-08-10 18:47:22 +0200

    Normalize options from input

---
+++ test/_options/normalize.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var isCopy  = require('es5-ext/lib/Object/is-copy')
+  , inspect = require('util').inspect;
+
+module.exports = function (t, a) {
+	var x = { foo: true }, copy;
+
+	a.not(t(x), x, "Return copy");
+	x = t({ foo: false, bar: {}, raz: ['raz', 3, {}, 'dwa'] });
+	copy = { foo: false, raz: ['raz', '3', '[object Object]', 'dwa']};
+
+	// console.log("OPTS", inspect(x, false, Infinity));
+	// console.log("COPY", inspect(copy, false, Infinity));
+	a(isCopy(x, copy, Infinity), true);
+};
