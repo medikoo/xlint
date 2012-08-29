cbf715c78e6aff6c90a3e81ea0d9bcabbd99ac3b
Mariusz Nowak <mariusz@medikoo.com>
2012-08-29 17:29:13 +0200

    Windows support

--- test/_options/parse.js
+++ test/_options/parse.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var isCopy   = require('es5-ext/lib/Object/is-copy')
-  , resolve  = require('path').resolve
+  , path     = require('path')
   , readFile = require('fs').readFile
   , inspect  = require('util').inspect
+
+  , join = path.join, resolve = path.resolve
 
   , testFile = resolve(__dirname, 'parse.txt');
 
@@ -14,31 +16,31 @@ module.exports = function (t, a, d) {
 			d(err);
 			return;
 		}
-		result = t(data, '/one/two');
+		result = t(data, join('/one/two'));
 		mod1 = [{ action: 'add', value: ['bar', 'foo'] }];
 		mod2 = [{ action: 'remove', value: ['mala', 'morda'] }];
 		mod1.mod = mod2.mod = true;
 
-		copy = {
-			root: true,
-			"/one/two": {
-				adsafe: false,
-				bitwise: true,
-				devel: true,
-				debug: false,
-				predef: ['two'],
-				eqeq: true
-			},
-			"/one/two/foo/bar": {
-				browser: false,
-				cap: true,
-				predef: mod1
-			},
-			"/one/two/marko-zagalo.js": {
-				predef: mod2
-			}
+		copy = { root: true };
+		copy[join('/one/two')] = {
+			adsafe: false,
+			bitwise: true,
+			devel: true,
+			debug: false,
+			predef: ['two'],
+			eqeq: true
+		};
+		copy[join('/one/two/foo/bar')] = {
+			browser: false,
+			cap: true,
+			predef: mod1
+		};
+		copy[join('/one/two/marko-zagalo.js')] = {
+			predef: mod2
 		};
 
+		// console.log("DATA", inspect(result, false, Infinity));
+		// console.log("COPY", inspect(copy, false, Infinity));
 		a(isCopy(result, copy, Infinity), true);
 		d();
 	});
