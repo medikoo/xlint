08d87ac6469895ed77165673b9e0c19764154852
Mariusz Nowak <mariusz@medikoo.com>
2012-09-18 14:39:03 +0200

    No trailing whitespace in place of she-bang

--- test/_normalize-source.js
+++ test/_normalize-source.js
@@ -3,8 +3,8 @@
 module.exports = function (t, a) {
 	a(t('\ufeffvar x = 4\n'), 'var x = 4\n', "BOM");
 	a(t('#!/usr/bin/env node\nvar x = 4\n'),
-		'//                 \nvar x = 4\n', "She bang");
+		'//                $\nvar x = 4\n', "She bang");
 	a(t('\ufeff#!/usr/bin/env node\nvar x = 4\n'),
-		'//                 \nvar x = 4\n', "BOM & She bang");
+		'//                $\nvar x = 4\n', "BOM & She bang");
 	a(t('var x = 4\n'), 'var x = 4\n', "Plain");
 };
