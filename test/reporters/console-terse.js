8b059278b2e558c2d6532cae0c559f6d830c06fa
Mariusz Nowak <mariusz@medikoo.com>
2013-02-03 14:53:49 +0100

    Support multiple linters in terse mode

---
+++ test/reporters/console-terse.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var deferred = require('deferred');
+
+module.exports = function (t, a) {
+	var report = [{ message: 'Lorem ipsum', at: 1, line: 1 }], invoked;
+	report.src = 'Lorem ipsum';
+	report.options = {};
+	t([deferred({ 'foo': report })], { log: function (str) {
+		a(typeof str, 'string', "Log string");
+		invoked = true;
+	} }).end();
+	a(invoked, true);
+};
