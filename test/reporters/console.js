7d78bf71d1369a59f821fc20eef396fde2100ab0
Mariusz Nowak <mariusz@medikoo.com>
2013-02-03 14:53:41 +0100

    Linter defined via '--linter' option
    Support multiple linters in ters mode

--- test/reporters/console.js
+++ test/reporters/console.js
@@ -6,7 +6,7 @@ module.exports = function (t, a) {
 	var report = [{ message: 'Lorem ipsum', at: 1, line: 1 }], invoked;
 	report.src = 'Lorem ipsum';
 	report.options = {};
-	t(deferred({ 'foo': report }), { log: function (str) {
+	t([deferred({ 'foo': report })], { log: function (str) {
 		a(typeof str, 'string', "Log string");
 		invoked = true;
 	} }).end();
