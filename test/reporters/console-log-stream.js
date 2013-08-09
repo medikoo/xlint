142027351433d81580c8721b8605d8d0911d2f92
Mariusz Nowak <mariusz@medikoo.com>
2013-08-09 17:20:12 +0200

    Configure log stream view

---
+++ test/reporters/console-log-stream.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var deferred = require('deferred');
+
+module.exports = function (t, a, d) {
+	var report = [{ message: 'Lorem ipsum', character: 1, line: 1 }], invoked
+	  , promise;
+	report.src = 'Lorem ipsum';
+	report.options = {};
+	promise = deferred({ 'foo': report });
+	t(promise, { log: function (str) {
+		a(typeof str, 'string', "Log string");
+		invoked = true;
+	} }).end();
+	promise.emit('change', { type: 'add', name: 'foo', report: report });
+	a(invoked, true);
+	process.nextTick(d);
+};
