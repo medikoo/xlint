5265a6600972dbb88252e2dfa0ec070906a04131
Mariusz Nowak <mariusz@medikoo.com>
2014-04-27 18:09:03 +0200

    Update up to changes in fs2 package

--- test/_lintignore-mode.js
+++ test/_lintignore-mode.js
@@ -5,7 +5,7 @@ var deferred = require('deferred')
   , path     = resolve(__dirname, '__playground/_lintignore-mode');
 
 module.exports = function (t, a) {
-	var mode = require('fs2/lib/_ignore-modes').lint;
+	var mode = require('fs2/lib/ignore-modes').lint;
 
 	a(mode.filename, '.lintignore', "Filename");
 	return {
